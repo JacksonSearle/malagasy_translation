d537c6c55096f1d51fb571fb6841bd8ffe55b0b5
Jackson Searle <jacksonsearle24@gmail.com>
2023-12-19 16:53:30 -0700

    ready for supercomputer

--- scripts/malagasy.sh
+++ scripts/malagasy.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00   # walltime
+#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --qos=cs
+
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
 python3 ../train_model.py \
     --activation-dropout 0.1 \
     --checkpoint-activations False \
-    --dropout 0.1 \
+    --dropout 0.0 \
     --embed-dim 16 \
     --ffn-dim 16 \
     --fsdp True \
@@ -12,6 +27,6 @@ python3 ../train_model.py \
     --seq-len 256 \
     --value-embed-dim 16 \
     --vocab-size 1000 \
-    --device cpu \
-    --batch-size 16 \
+    --device cuda \
+    --batch-size 1 \
     --epochs 10000 \
